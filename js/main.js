e1d9a09edae2e39e3692c36da4c4c4840be2d76c
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-05-31 18:38:38 +0000

    Added creation of entry through DOM, fixed html formatting. #2

--- js/main.js
+++ js/main.js
@@ -27,5 +27,44 @@ function saveEntryInfo(event) {
 
   $formImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
-
 }
+
+function createEntry(entry) {
+  //  <li>
+  //    <div class="row">
+  //      <div class="column-half">
+  //        <img>
+  //      </div>
+  //      <div class="column-half">
+  //        <h2></h2>
+  //        <p></p>
+  //      </div>
+  //    </div>
+  //  </li>
+
+  var $listItem = document.createElement('li');
+  var $row = document.createElement('div');
+  $row.className('row');
+  $listItem.appendChild($row);
+  var $imgCol = document.createElement('div');
+  $imgCol.className('column-half');
+  $row.appendChild($imgCol);
+  var $image = document.createElement('img');
+  $imgCol.appendChild($image);
+  var $textCol = document.createElement('div');
+  $textCol.className('column-half');
+  $row.appendChild($textCol);
+  var $entryHeader = document.createElement('h2');
+  var $entryText = document.createElement('p');
+  $textCol.appendChild($entryHeader);
+  $textCol.appendChild($entryText);
+
+  $image.setAttribute('src', entry.photoUrl);
+  $entryHeader.textContent = entry.title;
+  $entryText.textContent = entry.notes;
+
+  var unorderedList = document.querySelector('.entryList');
+  unorderedList.appendChild($listItem);
+}
+
+createEntry(data.entry[0]);
