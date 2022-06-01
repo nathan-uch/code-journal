954112b6dc8f60e0a9b1d19c0d3a801c653df309
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-06-01 23:50:46 +0000

    Updated submit form button for editing entries and html after submitting. #3

--- js/main.js
+++ js/main.js
@@ -6,34 +6,43 @@ var $formImage = document.querySelector('.form-img');
 
 $photoUrl.addEventListener('input', updatePhoto);
 
-function updatePhoto(event) {
-  if (event.target.value !== '') {
-    $formImage.setAttribute('src', event.target.value);
+function updatePhoto() {
+  if ($photoUrl.value !== '') {
+    $formImage.setAttribute('src', $photoUrl.value);
   }
 }
 
 var $entryTitle = document.querySelector('#title');
 var $entryNotes = document.querySelector('#notes');
 
+// SUBMIT
 $form.addEventListener('submit', saveEntryInfo);
 
 function saveEntryInfo(event) {
-  var currentEntry = {
-    title: $entryTitle.value,
-    photoUrl: $photoUrl.value,
-    notes: $entryNotes.value,
-    entryId: data.nextEntryId
-  };
-
-  data.entries.push(currentEntry);
-  data.nextEntryId++;
-
-  $formImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+  if (data.editing === null) {
+    var currentEntry = {
+      title: $entryTitle.value,
+      photoUrl: $photoUrl.value,
+      notes: $entryNotes.value,
+      entryId: data.nextEntryId
+    };
+    data.entries.push(currentEntry);
+    data.nextEntryId++;
+    loadEntry(currentEntry);
+  } else {
+    data.editing.title = $entryTitle.value;
+    data.editing.photoUrl = $photoUrl.value;
+    data.editing.notes = $entryNotes.value;
+    var currentLi = document.querySelectorAll('.entry-item');
+    for (var y = 0; y < currentLi.length; y++) {
+      if (data.editing.entryId === +currentLi[y].getAttribute('data-entry-id')) {
+        currentLi[y].replaceWith(renderEntry(data.editing));
+      }
+    }
+  }
   $form.reset();
-
   data.view = 'entries';
-  updateView(data.view);
-  loadEntry(currentEntry);
+  updateDataView(data.view);
 }
 
 function renderEntry(entry) {
@@ -88,16 +97,16 @@ window.addEventListener('DOMContentLoaded', loadPreviousEntries);
 
 function loadPreviousEntries() {
   for (var i = 0; i < data.entries.length; i++) {
-    var newEntry = renderEntry(data.entries[i]);
+    var newEntry = (data.entries[i]);
     $unorderedList.prepend(newEntry);
   }
-  updateView(data.view);
+  updateDataView(data.view);
 }
 
 function loadEntry(entry) {
   var newEntry = renderEntry(entry);
   $unorderedList.prepend(newEntry);
-  updateView(data.view);
+  updateDataView(data.view);
 }
 
 var $newEntryBtn = document.querySelector('.new-entry-a');
@@ -105,7 +114,7 @@ var $navbarLinks = document.querySelector('.navbar-link');
 var $formContainer = document.querySelector('[data-view="entry-form"]');
 var $entriesContainer = document.querySelector('[data-view="entries"]');
 
-function updateView(view) {
+function updateDataView(view) {
   if (view === 'entry-form') {
     $formContainer.className = 'active';
     $entriesContainer.className = 'hidden';
@@ -116,14 +125,17 @@ function updateView(view) {
 }
 
 $newEntryBtn.addEventListener('click', function (event) {
+  data.editing = null;
+  $formImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
   data.view = 'entry-form';
-  updateView(data.view);
+  updateDataView(data.view);
 });
 
 $navbarLinks.addEventListener('click', function (event) {
   if (event.target.textContent === 'Entries') {
     data.view = 'entries';
-    updateView(data.view);
+    updateDataView(data.view);
   }
 });
 
@@ -131,19 +143,21 @@ $unorderedList.addEventListener('click', findEditEntry);
 
 function findEditEntry(event) {
   if (event.target.tagName === 'I') {
-    data.view = 'entry-form';
-    updateView(data.view);
-    for (var i = 0; i < data.entries.length; i++) {
-      if (event.target.dataset.entryId === data.entries[i].entryID) {
-        data.editing = data.entries[i];
-        editEntry(data.editing);
+    var liId = +event.target.closest('li').getAttribute('data-entry-id');
+    for (var k = 0; k < data.entries.length; k++) {
+      if (liId === data.entries[k].entryId) {
+        data.editing = data.entries[k];
+        loadEditEntryPage(data.entries[k]);
       }
     }
   }
 }
 
-function editEntry(entry) {
+function loadEditEntryPage(entry) {
   $photoUrl.value = entry.photoUrl;
   $entryTitle.value = entry.title;
   $entryNotes.value = entry.notes;
+  updatePhoto();
+  data.view = 'entry-form';
+  updateDataView(data.view);
 }
